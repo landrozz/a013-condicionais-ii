5a5ade9830422e0b317c5509c47adafd1a5a8303
landrozz <leandrozanobine@gmail.com>
2022-12-12 15:21:14 -0300

    revisando e enviando

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,13 +1,20 @@
-//código a ser reescrito
+const nome = "Leandro"
 
-if(nome === "José"){
-	console.log("Oi, Zé!");
-} else {
-	console.log("Olá, "+nome);
-}
+nome === "José" ? console.log("Oi, Zé!") : console.log("Olá, "+nome)
 
-if(idade >= 18){
-	console.log("pode tirar carteira de motorista!");
-} else {
-	console.log("Ainda não pode tirar carteira de motorista");
-}
+// if(nome === "José"){
+// 	console.log("Oi, Zé!");
+// } else {
+// 	console.log("Olá, "+nome);
+// }
+
+
+const idade = 12
+
+idade >= 18 ? console.log("pode tirar carteira de motorista!") : console.log("Ainda não pode tirar carteira de motorista")
+
+// if(idade >= 18){
+// 	console.log("pode tirar carteira de motorista!");
+// } else {
+// 	console.log("Ainda não pode tirar carteira de motorista");
+// }
